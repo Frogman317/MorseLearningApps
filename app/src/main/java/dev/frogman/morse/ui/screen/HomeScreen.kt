58a4e7e90555c0b28eabfce036dfe62323ad129b
Yuma Nishino <98700444+Frogman317@users.noreply.github.com>
2024-12-07 16:15:31 +0900

    HomeScreen

---
+++ app/src/main/java/dev/frogman/morse/ui/screen/HomeScreen.kt
@@ -0,0 +1,32 @@
+package dev.frogman.morse.ui.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import dev.frogman.morse.ui.component.BottomNavigationBar
+import dev.frogman.morse.ui.navigation.MainNavigation
+
+@Composable
+fun HomeScreen() {
+
+    val navController: NavHostController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+
+        content = {innerPadding ->
+            MainNavigation(
+                modifier = Modifier.padding(innerPadding),
+                navController = navController,
+            )
+        },
+
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+    )
+}
