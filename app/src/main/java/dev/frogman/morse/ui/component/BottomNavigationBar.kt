8595572d987608169bacb7d4339bc67f9ae1bdaa
Yuma Nishino <98700444+Frogman317@users.noreply.github.com>
2024-12-07 16:15:16 +0900

    BottomBar

---
+++ app/src/main/java/dev/frogman/morse/ui/component/BottomNavigationBar.kt
@@ -0,0 +1,14 @@
+package dev.frogman.morse.ui.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    Text(currentRoute ?: "null")
+}
